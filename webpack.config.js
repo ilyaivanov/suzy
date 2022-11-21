c041f892d18d222851a707f77c46e1af83d97481
Ilya Ivanov <static.ila@gmail.com>
2022-11-21 13:49:17 +0200

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = (env, argv) => ({
   plugins: [
     new HtmlWebpackPlugin({
       template: "public/index.html",
+      favicon: "public/favicon.svg",
     }),
   ],
   resolve: { extensions: [".ts", ".tsx", ".js", ".json"] },
